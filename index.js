e73490f14a8ac7e0dd53b3c9d535ea769eff88da
원준 유 <youwjune@naver.com>
2020-08-13 01:23:55 +0900

    day 2

--- index.js
+++ index.js
@@ -43,7 +43,67 @@ wonjunInfo.age = 22; // value값 변경 가능함.
 console.log(wonjunInfo.gender);
 
 function sayHello(name, age){
-    console.log("Hello!", potato);
+    console.log("Hello!" + name + "you are" + age + "years old");
+    console.log(`Hello ${name} you are ${age} years old`)
 }
 
 sayHello("Wonjun",21);
+
+const greetWonjun = sayHello("Wonjun", 21);
+
+console.log(greetWonjun)
+
+//calculator
+
+const calculator = {
+    plus : function(a,b){
+        return a + b ;
+    }
+}
+
+const plus = calculator.plus(5,5)
+console.log(plus)
+
+//document 객체는 window 객체의 하위 object로 html 태그 조작을 담당한다.
+//getElementById -> html에서 해당 아이디를 가진 태그를 선택한다.
+//getElementByClassName, getElementByTagName
+
+//document.querySelector -> css 선택자 id는 #, 클래스는 .
+const title = document.getElementById("title");
+console.log(title);
+console.dir(title);
+//javascript로 HTML ID를 활용하여 HTML, CSS속성을 변경할 수 있다.
+title.style.color = "white";
+title.innerHTML = "HI! From JS";
+
+//window
+//javascript is waiting for occuring events.
+
+function handleResize(){
+    console.log("I have been resized")
+}
+window.addEventListener("resize", handleResize)
+
+function handleClick(){
+    console.log("Clicked");
+    title.style.color = "skyblue";
+}
+title.addEventListener("click", handleClick)
+
+//condition
+// && and || or
+if(20 > 5 && "nicolas" === "nicolas"){
+    console.log("yes")
+} else{
+    console.log("no")
+}
+
+//prompt 지금은 사용하지 않음.
+const age = prompt("How old are you? ")
+console.log(age)
+
+if (age >= 20){
+    console.log('You can drink')
+} else{
+    console.log('You cannot drink')
+}
