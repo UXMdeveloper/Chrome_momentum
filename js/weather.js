e356e8de26e3b51dbb09fc9fc8d2e32446da3891
원준 유 <youwjune@naver.com>
2020-08-18 03:27:53 +0900

    javascript development complete

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,17 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY = "379f901d255c3c997dc259e19345a23c";
 const COORDS = "coords";
+
+function getWeather(lat,lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json()
+    }).then(function(json){
+        const temperature =json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS,JSON.stringify(coordsObj));
@@ -14,6 +26,7 @@ function handleGeoSuccess(position){
         longitude : longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -25,11 +38,13 @@ function askForCoords(){
 }
 
 function loadCoords(){
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords === null){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
         askForCoords();
     } else{
-        //getWeather
+        const parseCoords = JSON.parse(loadedCoords);
+        console.log(parseCoords)
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
